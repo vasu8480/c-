80c36fc16f99606268bdff21ab202a5af49fe8ae
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-04-09 17:44:51 +0530

    Create DecimaltoOctal.cpp

---
+++ binary_numbers/DecimaltoOctal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int DecimaltoOctal(int n)
+{
+    int ans=0,x=1;
+    while(x<=n)
+        x*=8;
+    x/=8;
+    while(x>0)
+    {
+        int lastdigit =n/x;
+        n-=lastdigit*x;
+        x/=8;
+        ans=ans*10+lastdigit;
+    }
+    return ans;
+}
+int main() 
+{    
+    int n;
+    cin>> n;
+    cout<<DecimaltoOctal(n);
+}
